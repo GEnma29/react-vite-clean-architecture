f3c9e2217fbab779ddbcb47aec024ab127a6b41f
Enmanuel Alfonzo <14-05775@usb.ve>
2022-12-19 07:51:48 -0400

    validation

--- src/routers/main.routes.tsx
+++ src/routers/main.routes.tsx
@@ -3,13 +3,15 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 // pages 
 import MainPage from '~/pages/main.page'
 import TestPage from '~/pages/test.page'
-
+import { PublicRoutes} from '~/routers/routers'
 const MainRoutes = () => {
   return (
     <BrowserRouter>
     <Routes>
         <Route path='/' element={<MainPage />} />
-        <Route path='/test' element={<TestPage />} />
+        <Route path='*' element={<h1>404</h1>} />
+        <Route path={PublicRoutes.Test} element={<TestPage />} />
+        <Route path={PublicRoutes.Main} element={<MainPage />} />
     </Routes>
     </BrowserRouter>
   )
---
+++ src/routers/routers.ts
@@ -0,0 +1,4 @@
+export const PublicRoutes = {
+    Main: 'Main',
+    Test: 'Test',
+}
